3e0c75de0f8e87ca00a704c826e05658569c581e
Mone Zhao <zym@ciphergateway.com>
2017-08-31 15:21:44 +0800

    提取index和type, 增加build的 Java language level

--- src/main/java/com/monezhao/App.java
+++ src/main/java/com/monezhao/App.java
@@ -14,6 +14,7 @@ import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
@@ -21,17 +22,22 @@ import org.elasticsearch.search.SearchHits;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Hello world!
  */
 public class App {
 
-  public static void main(String[] args) {
+//  public static final String index = "dashboard";
+    public static final String index = "megacorp";
+//  public static final String type = "dashboard";
+  public static final String type = "employee";
+
+  public static void main(String[] args) throws IOException {
     Log4jESLoggerFactory.getRootLogger().setLevel("ERROR");
     Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", "ciphergateway").build();
     Client client = new TransportClient(settings)
@@ -43,24 +49,38 @@ public class App {
     client.close();
   }
 
-  private static void importES(Client client) {
+  private static void importES(Client client) throws IOException {
     String filePath = "es.txt";
     File file = new File(filePath);
     if (!file.exists()) {
       System.out.println("文件不存在");
       return;
     }
-    try {
+    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
       //读取刚才导出的ES数据
-      BufferedReader br = new BufferedReader(new FileReader(filePath));
-      String json;
+
+      String string;
       int count = 0;
       //开启批量插入
       BulkRequestBuilder bulkRequest = client.prepareBulk();
-      while ((json = br.readLine()) != null) {
-        System.out.println(json);
+      while ((string = br.readLine()) != null) {
+        if (Objects.equals(string, "")) continue;
+        JsonObject jsonObject = new JsonObject(string);
+        System.out.println(jsonObject);
+        String ESindex;
+        if (jsonObject.containsKey("ESindex")) {
+          ESindex = (String) jsonObject.remove("ESindex");
+        } else {
+          ESindex = index;
+        }
+        String EStype;
+        if (jsonObject.containsKey("EStype")) {
+          EStype = (String) jsonObject.remove("EStype");
+        } else {
+          EStype = type;
+        }
         ++count;
-        bulkRequest.add(client.prepareIndex("megacorp", "employee", String.valueOf(count)).setSource(json));
+        bulkRequest.add(client.prepareIndex(ESindex, EStype, String.valueOf(count)).setSource(jsonObject.toString()));
         //每一千条提交一次
         if (count % 1000 == 0) {
           bulkRequest.execute().actionGet();
@@ -69,17 +89,12 @@ public class App {
       }
       bulkRequest.execute().actionGet();
       System.out.println("插入完毕");
-      br.close();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
     }
   }
 
   private static void deleteES(Client client) {
     BulkRequestBuilder bulkRequest = client.prepareBulk();
-    SearchResponse response = client.prepareSearch("megacorp").setTypes("employee")
+    SearchResponse response = client.prepareSearch(index).setTypes(type)
         .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
         .setQuery(QueryBuilders.matchAllQuery())
         .setFrom(0).setSize(20).setExplain(true).execute().actionGet();
@@ -88,7 +103,7 @@ public class App {
       for (SearchHit hit : response.getHits()) {
         String id = hit.getId();
         System.out.println("id: " + id);
-        bulkRequest.add(client.prepareDelete("megacorp", "employee", id).request());
+        bulkRequest.add(client.prepareDelete(index, type, id).request());
       }
       BulkResponse bulkResponse = bulkRequest.get();
       if (bulkResponse.hasFailures()) {
@@ -103,24 +118,26 @@ public class App {
     }
   }
 
-  private static void exportES(Client client) {
-    SearchResponse response = client.prepareSearch("megacorp").setTypes("employee")
-        .setQuery(QueryBuilders.matchAllQuery()).setSize(10000).setScroll(new TimeValue(600000))
+  private static void exportES(Client client) throws IOException {
+    SearchResponse response = client.prepareSearch(index).setTypes(type)
+        .setQuery(
+            QueryBuilders.filteredQuery(
+                QueryBuilders.matchPhraseQuery("operation", "账号密码登录"),
+                FilterBuilders.rangeFilter("opertime").from(1494916514776L).to(1494916556062L)
+                ))
+//        .addSort(SortBuilders.fieldSort("opertime"))
+        .setSize(10000)
+        .setScroll(new TimeValue(600000))
         .setSearchType(SearchType.SCAN).execute().actionGet();//setSearchType(SearchType.Scan) 告诉ES不需要排序只要结果返回即可 setScroll(new TimeValue(600000)) 设置滚动的时间
     String scrollid = response.getScrollId();
 
     String filePath = "es.txt";
-
     File file = new File(filePath);
-    try {
-      System.out.println(file.getAbsolutePath());
-      if (!file.exists()) {
-        System.out.println("create file " + file.createNewFile());
-      }
-      //把导出的结果以JSON的格式写到文件里
-      BufferedWriter out = new BufferedWriter(new FileWriter(filePath, true));
-
-      //每次返回数据10000条。一直循环查询直到所有的数据都查询出来
+    System.out.println(file.getAbsolutePath());
+    System.out.println("delete file " + file.delete());
+    System.out.println("create file " + file.createNewFile());
+    try (BufferedWriter out = new BufferedWriter(new FileWriter(filePath, true))) {
+      //把导出的结果以JSON的格式写到文件里,每次返回数据10000条。一直循环查询直到所有的数据都查询出来
       while (true) {
         SearchResponse response2 = client.prepareSearchScroll(scrollid).setScroll(new TimeValue(1000000))
             .execute().actionGet();
@@ -130,15 +147,17 @@ public class App {
           break;
         }
         for (int i = 0; i < searchHit.getHits().length; i++) {
-          out.write(new JsonObject(searchHit.getHits()[i].getSourceAsString()).toString());
+          SearchHit hit = searchHit.getHits()[i];
+          System.out.println(hit.getSourceAsString());
+          out.write(
+              new JsonObject(hit.getSourceAsString())
+                  .put("ESindex", hit.index())
+                  .put("EStype", hit.type())
+                  .toString()
+          );
           out.write("\n");
         }
       }
-      out.close();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
     }
   }
 }
